889a04b3940cc3827a3cbd2a09c7b90eb03fd1a8
d1catt <kociecki@wp.pl>
2015-10-03 18:26:48 +0200

    setup opts

--- readCSV.js
+++ readCSV.js
@@ -1,2 +1,15 @@
-'use strict';
-const csv-parser = require('csv-parser');
+var csv = require('csv-parser');
+var fs = require('fs');
+
+var opts = {
+  raw: false, // do not decode to utf-8 strings
+  separator: ';', // specify optional cell separator
+  newline: '\n', // specify a newline character
+  headers: ['index', 'message'] // Specifing the headers
+};
+
+fs.createReadStream('some.csv')
+  .pipe(csv(opts))
+  .on('data', function(data) {
+    console.log('row', data);
+  });
